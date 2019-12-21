00b71b3f77267a70c10ad5b3b5d3caffb39237ff
Johan Broberg <johanb@microsoft.com>
2019-12-21 00:27:31 -0800

    Added stub for Day 12 Puzzle 2.

---
+++ AdventOfCode.Test/Year2019/Day12Puzzle2Tests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pontemonti.AdventOfCode.Test.Year2019
+{
+    [TestClass]
+    public class Day12Puzzle2Tests
+    {
+        [TestMethod]
+        public void TestSolve()
+        {
+
+        }
+    }
+}
