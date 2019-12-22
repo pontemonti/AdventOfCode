1583d5575c9d4fa12f89de4f1695b8cc8e95fe01
Johan Broberg <johanb@microsoft.com>
2019-12-21 18:11:57 -0800

    Added stub for Day 13 Puzzle 1.

---
+++ AdventOfCode.Test/Year2019/Day13Puzzle1Tests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pontemonti.AdventOfCode.Test.Year2019
+{
+    [TestClass]
+    public class Day13Puzzle1Tests
+    {
+        [TestMethod]
+        public void TestSolve()
+        {
+
+        }
+    }
+}
