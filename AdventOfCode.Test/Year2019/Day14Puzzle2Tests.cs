86391c154be6604db287318cde0664ee733b03be
Johan Broberg <johanb@microsoft.com>
2019-12-28 20:27:48 -0800

    Added stub for Day 14 Puzzle 2.

---
+++ AdventOfCode.Test/Year2019/Day14Puzzle2Tests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pontemonti.AdventOfCode.Test.Year2019
+{
+    [TestClass]
+    public class Day14Puzzle2Tests
+    {
+        [TestMethod]
+        public void TestSolve()
+        {
+
+        }
+    }
+}
